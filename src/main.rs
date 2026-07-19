cdc325d1b82f3ff47622bb68ffbc54793202af25
metal engineer <me@mydomain.com>
2026-07-19 19:08:05 +0100

    mid

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
+use std::{thread, time::Duration};
+
 use embedded_graphics::{
-    pixelcolor::Rgb565,
+    pixelcolor::{BinaryColor, Rgb565},
     prelude::*,
-    primitives::{PrimitiveStyle, Rectangle, StyledDrawable, Triangle},
+    primitives::{PrimitiveStyle, Triangle},
 };
 //use embedded_graphics_simulator::BinaryColorTheme;
 use embedded_graphics_simulator::{
     OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
-
-struct Q {
-    q_x: i32,
-    q_y: i32,
-    minus_d: i32,
-}
+use libm::{atan, cos, sin};
 
 const DISP_SIZE: u32 = 400;
 fn main() -> Result<(), std::convert::Infallible> {
@@ -20,43 +17,57 @@ fn main() -> Result<(), std::convert::Infallible> {
         SimulatorDisplay::new(Size::new(DISP_SIZE, DISP_SIZE));
     let output_settings = OutputSettingsBuilder::new().scale(2).build();
     let mut window = Window::new("Graph", &output_settings);
-    let stroke = PrimitiveStyle::with_stroke(Rgb565::WHITE, 3);
-    let mid_point = DISP_SIZE as i32 / 2;
-
-    let Ps: Vec<Vec<i32>> = vec![
-        vec![0, 0, 0],
-        vec![0, 1, 0],
-        vec![1, 0, 0],
-        vec![1, 1, 0],
-        vec![0, 0, 1],
-        vec![0, 1, 1],
-        vec![1, 0, 1],
-        vec![1, 1, 1],
+    let z_offset = -5.0;
+    let mut x_offset = 0.0;
+    let mut y_offset = 0.0;
+    let scale = 50.0;
+    let ps: Vec<Vec<f32>> = vec![
+        vec![1.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0],
+        vec![0.0, 1.0, 0.0],
+        vec![1.0, 1.0, 0.0],
+        vec![0.0, 0.0, 1.0],
+        vec![0.0, 1.0, 1.0],
+        vec![1.0, 0.0, 1.0],
+        vec![1.0, 1.0, 1.0],
     ];
 
-    'running: loop {
-        let mut Qs: Vec<Vec<i32>> = Vec::new();
-        let minus_d = -1.0;
-        let z_offset = -5.0;
-        for i in Ps.iter() {
-            let px = i[0] as f32;
-            let py = i[1] as f32;
-            let pz = i[2] as f32 + z_offset;
-            let q_x = (600.0 * (minus_d * px) / pz) as i32 + 50;
-            let q_y = (600.0 * (minus_d * py) / pz) as i32 + 50;
-            Qs.push(vec![q_x, q_y]);
-        }
+    let mut Qs: Vec<Vec<i32>> = Vec::new();
 
+    //let vert_points: Vec<Vec<f32>> = Vec::new();
+
+    let mut rot: f64 = 30.0;
+    for i in ps.iter() {
+        let px = i[0] as f32;
+        let py = i[1] as f32;
+        let pz = i[2] as f32 + z_offset;
+
+        if px == 1.0 && py == 0.0 && pz + 5.0 == 0.0 {
+            let q_x = (scale * (px - pz) * cos((rot).to_radians()) as f32) as i32;
+            let q_y = (scale * ((px + pz) * sin((rot).to_radians()) as f32 - py)) as i32;
+
+            println!("q_x {} q_y {}", q_x, q_y);
+            x_offset = (DISP_SIZE as i32 / 2 - q_x) as f32;
+            y_offset = (DISP_SIZE as i32 / 2 - q_y) as f32;
+            println!("x_offset {} y_offset {}", x_offset, y_offset);
+        }
+        let q_x = (scale * (px - pz) * cos((rot).to_radians()) as f32 + x_offset) as i32;
+        let q_y = (scale * ((px + pz) * sin((rot).to_radians()) as f32 - py) + y_offset) as i32;
+        let q_z = pz as i32;
+        println!("q_x {} q_y {} q_z {}", q_x, q_y, q_z);
+
+        Qs.push(vec![q_x, q_y]);
+    }
+    'running: loop {
         let _ = display.clear(Rgb565::new(1, 1, 1));
 
-        for q in Qs.iter() {
-            Rectangle::new(Point::new(q[0], q[1]), Size::new(3, 3))
-                .into_styled(stroke)
-                .draw(&mut display)?;
+        for i in Qs.iter() {
+            Pixel(Point::new(i[0], i[1]), Rgb565::WHITE).draw(&mut display)?;
+            //Triangle::new(Point::new(i[0], i[1]),)
         }
 
+        //Qs.clear();
         window.update(&display);
-
         for e in window.events() {
             match e {
                 SimulatorEvent::Quit => {
@@ -65,5 +76,6 @@ fn main() -> Result<(), std::convert::Infallible> {
                 _ => {}
             }
         }
+        thread::sleep(Duration::from_millis(500));
     }
 }
