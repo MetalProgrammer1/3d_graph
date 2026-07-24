43ca56eb18e5d059ac09fed54cdc3cb687cfceda
metal engineer <me@mydomain.com>
2026-07-24 18:40:04 +0100

    new

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 pub mod transform;
 use embedded_graphics::{
-    pixelcolor::{BinaryColor, Rgb565},
+    pixelcolor::Rgb565,
     prelude::*,
-    primitives::{PrimitiveStyle, Rectangle, Triangle},
+    primitives::{Line, PrimitiveStyle, Rectangle, Triangle},
 };
 use std::{thread, time::Duration, vec};
-//use embedded_graphics_simulator::BinaryColorTheme;
+
 use embedded_graphics_simulator::{
     OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
-use libm::{atan, cos, sin, sinh};
+use libm::{cos, sin};
 
-use crate::transform::rotate_about_y;
+use crate::transform::{rotate_about_x, rotate_about_z};
 
 const DISP_SIZE: u32 = 500;
 fn main() -> Result<(), std::convert::Infallible> {
@@ -19,76 +19,63 @@ fn main() -> Result<(), std::convert::Infallible> {
         SimulatorDisplay::new(Size::new(DISP_SIZE, DISP_SIZE));
     let output_settings = OutputSettingsBuilder::new().scale(2).build();
     let mut window = Window::new("Graph", &output_settings);
-    let z_offset = -0.0;
-    let mut x_offset = 0;
+
     let mut y_offset = 0;
-    let scale = 1.0;
+
     let mut ps: Vec<Vec<f32>> = Vec::new();
     let mut qs: Vec<Vec<f32>> = Vec::new();
 
+    let mut grid: Vec<Vec<f32>> = Vec::new();
+
     //let vert_points: Vec<Vec<f32>> = Vec::new();
-    let spacing = 1;
+    let spacing = 3;
 
     for i in -100..100 {
         for j in -100..100 {
             let x = i as f32 * 0.03;
             let y = j as f32 * 0.03;
 
-            let z = x.powf(2.0) + y.powf(2.0);
-            let z1 = sin(x.powf(2.0) as f64) + cos(y.powf(2.0) as f64) as f64;
-            //let z = (-x.powf(2.0) - y.powf(2.0) + 20.0).powf(0.5);
+            let z = sin(x.powf(2.0) as f64) + cos(y.powf(2.0) as f64) as f64;
+
             if !z.is_nan() {
-                //ps.push(vec![x, y, z as f32]);
-                //ps.push(vec![x, y, z1 as f32]);
-                ps.push(vec![(i * spacing) as f32, (j * spacing) as f32, 0.0]);
-                //ps.push(vec![x, y, -z]);
+                ps.push(vec![x, y, z as f32]);
             }
         }
     }
-
+    for i in 0..20 {
+        for j in 0..20 {
+            grid.push(vec![(i * spacing) as f32, (j * spacing) as f32, 0.0]);
+        }
+    }
     let rot: f64 = 30.0;
     let mut phi_y: f64 = 0.0;
-    //let  phi_x: f64 = 0.0;
     'running: loop {
         let _ = display.clear(Rgb565::new(1, 1, 1));
 
         for i in ps.iter() {
             let px_og = i[0] as f32;
             let py_og = i[1] as f32;
-            let pz_og = i[2] as f32 + z_offset;
+            let pz_og = i[2] as f32;
 
-            // let px1 =
-            //     px_og * cos(phi_y.to_radians()) as f32 - py_og * sin(phi_y.to_radians()) as f32;
-            // let py1 =
-            //     px_og * sin(phi_y.to_radians()) as f32 + py_og * cos(phi_y.to_radians()) as f32;
-            // let pz1 = pz_og;
+            let ps_rot_z = rotate_about_z(phi_y, vec![px_og, py_og, pz_og]);
 
-            let ps_rotated = rotate_about_y(vec![px_og, py_og, pz_og]);
+            //let ps_rot_x = rotate_about_x(phi_y, vec![ps_rot_z[0], ps_rot_z[1], ps_rot_z[2]]);
 
-            let px = px1;
-            let py = py1 * cos(phi_y.to_radians()) as f32 - pz1 * sin(phi_y.to_radians()) as f32;
-            let pz = py1 * sin(phi_y.to_radians()) as f32 + pz1 * cos(phi_y.to_radians()) as f32;
-
-            //py = (cos(phi.to_radians()) as f32 * py - sin(phi.to_radians()) as f32 * pz) as f32;
-            //pz = (sin(phi.to_radians()) as f32 * py + cos(phi.to_radians()) as f32 * pz) as f32;
+            let px = ps_rot_z[0];
+            let py = ps_rot_z[1];
+            let pz = ps_rot_z[2];
 
             let q_x = ((px - py) * cos(rot.to_radians()) as f32 * 15.0) + 250.0;
-            let q_y = (((px + py) * sin(rot.to_radians()) as f32 - pz) * 15.0);
+            let q_y = ((px + py) * sin(rot.to_radians()) as f32 - pz) * 15.0;
             let q_z = pz;
 
-            //println!("q_x {} q_y {} q_z {}", q_x, q_y, q_z);
             if px_og == 0.0 && py_og == 0.0 && py_og == 0.0 {
                 y_offset = DISP_SIZE as i32 / 2 - q_y as i32;
             }
             qs.push(vec![q_x, q_y, q_z]);
         }
-        //let max: Vec<&Vec<i32>> = 0;
-        //println!("max {:?}", max);
-        //let y_offset = 0; //350 - max;
+
         phi_y += 5.0;
-        // if phi_y >= 1000.0 {
-        //     phi_y = 0.0;
-        // }
 
         qs.sort_by(|a, b| a[2].partial_cmp(&b[2]).unwrap());
         for i in qs.iter() {
@@ -104,7 +91,55 @@ fn main() -> Result<(), std::convert::Infallible> {
             .into_styled(PrimitiveStyle::with_fill(Rgb565::new(r, g, b)))
             .draw(&mut display)?;
         }
+        println!("{}", grid.len());
 
+        let center = rotate_about_z(phi_y, vec![27.0, 27.0, 0.0]);
+        let cpx = center[0];
+        let cpy = center[1];
+        let cpz = 0.0;
+        let cq_x = ((cpx - cpy) * cos(rot.to_radians()) as f32 * 15.0);
+        let cq_y = ((cpx + cpy) * sin(rot.to_radians()) as f32 - cpz) * 15.0;
+        let y_offset_grid = DISP_SIZE as i32 / 2 - cq_y as i32;
+        let x_offset_grid = DISP_SIZE as i32 / 2 - cq_x as i32;
+        let mut grid_points: Vec<Point> = Vec::new();
+        for i in 0..grid.len() {
+            let ps_rot_z = rotate_about_z(phi_y, vec![grid[i][0], grid[i][1], 0.0]);
+            //let ps_rot_x = rotate_about_x(phi_y, vec![ps_rot_z[0], ps_rot_z[1], ps_rot_z[2]]);
+            let px = ps_rot_z[0];
+            let py = ps_rot_z[1];
+            let pz = ps_rot_z[2];
+
+            let q_x = ((px - py) * cos(rot.to_radians()) as f32 * 15.0) as i32;
+            let q_y = (((px + py) * sin(rot.to_radians()) as f32 - pz) * 15.0) as i32;
+            let q_z = pz;
+
+            Rectangle::new(
+                Point::new(q_x + x_offset_grid, q_y + y_offset_grid),
+                Size::new(2, 2),
+            )
+            .into_styled(PrimitiveStyle::with_fill(Rgb565::new(255, 255, 255)))
+            .draw(&mut display)?;
+            grid_points.push(Point::new(q_x + x_offset_grid, q_y + y_offset_grid));
+        }
+
+        for i in 0..20 {
+            for j in 0..20 {
+                let idx = i * 20 + j;
+                if i + 1 < 20 {
+                    let idx_right = (i + 1) * 20 + j;
+                    Line::new(grid_points[idx], grid_points[idx_right])
+                        .into_styled(PrimitiveStyle::with_stroke(Rgb565::new(255, 255, 255), 1))
+                        .draw(&mut display)?;
+                }
+
+                if j + 1 < 20 {
+                    let idx_right = i + j * 20;
+                    Line::new(grid_points[idx], grid_points[idx_right])
+                        .into_styled(PrimitiveStyle::with_stroke(Rgb565::new(255, 255, 255), 1))
+                        .draw(&mut display)?;
+                }
+            }
+        }
         qs.clear();
         window.update(&display);
         for e in window.events() {
@@ -115,6 +150,6 @@ fn main() -> Result<(), std::convert::Infallible> {
                 _ => {}
             }
         }
-        thread::sleep(Duration::from_millis(50));
+        thread::sleep(Duration::from_millis(40));
     }
 }
--- src/transform.rs
+++ src/transform.rs
@@ -1,3 +1,23 @@
-pub fn rotate_about_y(points: Vec<f32>) -> Vec<f32> {
-    vec![0.0, 0.0, 0.0]
+use libm::{cos, sin};
+
+pub fn rotate_about_z(phi_y: f64, points: Vec<f32>) -> Vec<f32> {
+    let px_og = points[0];
+    let py_og = points[1];
+    let pz_og = points[2];
+    let px = px_og * cos(phi_y.to_radians()) as f32 - py_og * sin(phi_y.to_radians()) as f32;
+    let py = px_og * sin(phi_y.to_radians()) as f32 + py_og * cos(phi_y.to_radians()) as f32;
+    let pz = pz_og;
+    vec![px, py, pz]
+}
+
+pub fn rotate_about_x(phi_y: f64, points: Vec<f32>) -> Vec<f32> {
+    let px_og = points[0];
+    let py_og = points[1];
+    let pz_og = points[2];
+
+    let px = px_og;
+    let py = py_og * cos(phi_y.to_radians()) as f32 - pz_og * sin(phi_y.to_radians()) as f32;
+    let pz = py_og * sin(phi_y.to_radians()) as f32 + pz_og * cos(phi_y.to_radians()) as f32;
+
+    vec![px, py, pz]
 }
